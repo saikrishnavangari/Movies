f4c3a611d1c2a2ee361ba28a2a2adebbcf6bbe4c
saikrishnavangari <saikrishna.vangari@appscore.com.au>
2016-11-02 00:46:42 +1100

    Main activity formatted

--- app/src/main/java/sai/com/popularmovies/MainActivity.java
+++ app/src/main/java/sai/com/popularmovies/MainActivity.java
@@ -14,13 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-// Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
+
         if (findViewById(R.id.fragment_container) != null) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null) {
                 return;
             }
