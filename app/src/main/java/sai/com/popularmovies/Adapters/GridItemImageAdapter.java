6c7bc3fdc088defa2eed174ff6e2134fd101cab6
saikrishnavangari <saikrishna.vangari@appscore.com.au>
2016-11-28 13:30:19 +1100

    implemented viewholder pattern

--- app/src/main/java/sai/com/popularmovies/Adapters/GridItemImageAdapter.java
+++ app/src/main/java/sai/com/popularmovies/Adapters/GridItemImageAdapter.java
@@ -22,17 +22,15 @@ import sai.com.popularmovies.R;
  * Created by krrish on 1/11/2016.
  */
 
-public class
-GridItemImageAdapter extends ArrayAdapter<results> {
+public class GridItemImageAdapter extends ArrayAdapter<results> {
     private Context mcontext;
     private List<results> mMoviesList;
-    @BindView(R.id.movie_title)TextView title_tv;
-    @BindView(R.id.image_id)ImageView moviePoster;
+
 
     public GridItemImageAdapter(Context context, int resource, List<results> list) {
         super(context, resource, list);
-        mcontext=context;
-        mMoviesList=list;
+        mcontext = context;
+        mMoviesList = list;
     }
 
 
@@ -50,18 +48,36 @@ GridItemImageAdapter extends ArrayAdapter<results> {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        View listItemView=view;
-
-        if(listItemView==null){
+        View listItemView = view;
+        ViewHolder viewHolder;
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.griditemview, viewGroup, false);
+            viewHolder = new ViewHolder(listItemView);
+            listItemView.setTag(viewHolder);
         }
-        ButterKnife.bind(this,listItemView);
+        else
+        {
+            viewHolder= (ViewHolder) listItemView.getTag();
+        }
+
         //load image into imageView
-        Picasso.with(mcontext).load(MainActivity.IMAGE_BASE_URL+"w185/"+mMoviesList.get(i).getPoster_path())
-                .into(moviePoster);
-        title_tv.setText(mMoviesList.get(i).getOriginal_title());
+        Picasso.with(mcontext).load(MainActivity.IMAGE_BASE_URL + "w185/" + mMoviesList.get(i).getPoster_path())
+                .into(viewHolder.moviePoster);
+        viewHolder.title_tv.setText(mMoviesList.get(i).getOriginal_title());
         return listItemView;
+    }
+
+
+    static class ViewHolder {
+        @BindView(R.id.movie_title)
+        TextView title_tv;
+        @BindView(R.id.image_id)
+        ImageView moviePoster;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
 
